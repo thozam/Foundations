7126aa2cecac766db44df5df168352e5a01c3260
thozam <thomas@limpingstone.se>
2016-10-27 11:20:38 +0200

    Homework3a from Thomas Lindstrom

---
+++ Homework3a/Homework3a/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int result = 1;
+
+            Console.Write("Please enter a small integer: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n+1; i++)
+            {
+                result = result * i;
+            }
+
+            Console.Write("The factorial of {0} is: {1}", n, result);
+            Console.ReadLine();
+        }
+    }
+}

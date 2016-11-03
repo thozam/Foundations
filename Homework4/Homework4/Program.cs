485e924f50eda8706724d950c0821b1ce40cf323
thozam <thomas@limpingstone.se>
2016-11-03 14:45:49 +0100

    Homework 4

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int result = 1;
+
+            Console.Write("\nPlease enter a small integer: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                result = result * i;
+                Console.Write("\n{0}!\t=\t{1}", i, result);
+            }
+
+            Console.Write("\n\nThe factorial of {0} is: {1}", n, result);
+            Console.ReadLine();
+        }
+    }
+}
